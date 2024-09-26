dfe8935dfaab5ef00172420589cf1c143cf24675
Eugene <compserv@yahoo.com>
2024-09-27 01:02:38 +0300

    add 4 query

--- src/main/resources/task.sql
+++ src/main/resources/task.sql
@@ -1,25 +1,53 @@
 -- Вывести к каждому самолету класс обслуживания и количество мест этого класса
 SELECT aircrafts_data.aircraft_code,
        model,
-       aircrafts_data.range,
+       range,
        fare_conditions,
        count(seat_no)
-FROM bookings.aircrafts_data
-         INNER JOIN bookings.seats
-         ON bookings.aircrafts_data.aircraft_code = bookings.seats.aircraft_code
+FROM aircrafts_data
+         INNER JOIN seats
+                    ON bookings.aircrafts_data.aircraft_code = bookings.seats.aircraft_code
 group by aircrafts_data.aircraft_code,
          model,
-         aircrafts_data.range,
-         fare_conditions
+         range,
+         fare_conditions;
+
 -- Найти 3 самых вместительных самолета (модель + кол-во мест)
+select model,
+       count(seats.seat_no) count_seats
+FROM aircrafts_data
+         INNER JOIN seats
+                    ON bookings.aircrafts_data.aircraft_code = bookings.seats.aircraft_code
+group by model
+order by count_seats DESC
+limit 3;
 
 -- Вывести код, модель самолета и места не эконом класса для самолета 'Аэробус A321-200' с сортировкой по местам
+SELECT aircrafts_data.aircraft_code,
+       model,
+       seat_no
+FROM aircrafts_data
+         INNER JOIN seats
+                    ON bookings.aircrafts_data.aircraft_code = bookings.seats.aircraft_code
+WHERE fare_conditions not LIKE 'Economy'
+  AND model::text LIKE '%Аэробус A321-200%';
 
--- Вывести города в которых больше 1 аэропорта ( код аэропорта, аэропорт, город)
+
+-- Вывести города в которых больше 1 аэропорта (код аэропорта, аэропорт, город)
+SELECT airport_code,
+       airport_name,
+       city
+FROM airports_data
+    where city->>'ru' in (
+        SELECT city->>'ru'
+        FROM airports_data
+        GROUP BY city->>'ru'
+        HAVING COUNT(city) > 1)
+
 
 -- Найти ближайший вылетающий рейс из Екатеринбурга в Москву, на который еще не завершилась регистрация
 
--- Вывести самый дешевый и дорогой билет и стоимость ( в одном результирующем ответе)
+-- Вывести самый дешевый и дорогой билет и стоимость (в одном результирующем ответе)
 
 -- Вывести информацию о вылете с наибольшей суммарной стоимостью билетов
 
